a619a8dd97eeb1583e23533d6c9c46ce03b0d170
Huaiyue Luo <julijanaluo@gmail.com>
2025-10-28 22:26:19 +0100

    Update upload method to use upload_folder
    Replaced model push with folder upload to Hugging Face.

--- upload_hugging_face.py
+++ upload_hugging_face.py
@@ -1,16 +1,13 @@
 from huggingface_hub import HfApi
-from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 import os
 
 repo_id = "hyluo/atmt_a3_MQA"
 model_dir = "/data/atmt_2025_assignment3/cz-en"
-
 token = os.getenv("HF_TOKEN")
-if not token:
-    raise ValueError("❌ Missing HF_TOKEN environment variable")
 
-tokenizer = AutoTokenizer.from_pretrained(model_dir)
-model = AutoModelForSeq2SeqLM.from_pretrained(model_dir)
-
-tokenizer.push_to_hub(repo_id, token=token)
-model.push_to_hub(repo_id, token=token)
+api.upload_folder(
+    folder_path=model_dir,
+    repo_id=repo_id,
+    repo_type="model",
+    token=token
+)
