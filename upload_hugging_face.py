d2c586b38fd410fb0ffa1da92e982aaf872a63bb
Huaiyue Luo <julijanaluo@gmail.com>
2025-10-28 22:11:24 +0100

    Refactor model upload to use transformers and token
    Updated model upload process to use environment token and transformers library.

--- upload_hugging_face.py
+++ upload_hugging_face.py
@@ -1,16 +1,16 @@
-from huggingface_hub import login
 from huggingface_hub import HfApi
+from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
+import os
 
-# 需要在huggingface里面注册后获取的API
-login("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")
+repo_id = "hyluo/atmt_a3_MQA"
+model_dir = "/data/atmt_2025_assignment3/cz-en"
 
-api = HfApi()
-model_name = "hyLuo/atmt_MQA_model"  # 定义微调后的模型名称
-api.create_repo(model_name, exist_ok=True)  # 创建一个新的 Hugging Face 公开仓库
+token = os.getenv("HF_TOKEN")
+if not token:
+    raise ValueError("❌ Missing HF_TOKEN environment variable")
 
-api.upload_folder(
-    folder_path="./cz-en",  # 你的微调模型输出路径
-    repo_id=model_name,  # 你的 Hugging Face 仓库
-    repo_type="model"
-)
-print('模型上传成功')
+tokenizer = AutoTokenizer.from_pretrained(model_dir)
+model = AutoModelForSeq2SeqLM.from_pretrained(model_dir)
+
+tokenizer.push_to_hub(repo_id, token=token)
+model.push_to_hub(repo_id, token=token)
