236d1716f6f9dd5173b3c646b027de007a11fa37
Huaiyue Luo <julijanaluo@gmail.com>
2025-12-06 20:40:26 +0100

    Add alpha parameter to output command(default value)

--- toy_example.sh
+++ toy_example.sh
@@ -63,6 +63,7 @@ python translate.py \
     --checkpoint-path toy_example/checkpoints/checkpoint_best.pt \
     --batch-size 1 \
     --max-len 100 \
-    --output toy_example/toy_example_output.en \
+    --alpha \
+    --output toy_example/toy_example_output_alpha_default.en \
     --bleu \
     --reference toy_example/data/raw/test.en
